30a3483747bc338aa26ac3d749bab45ff6e00838
Amneesh Singh <amneesh@ti.com>
2026-03-11 15:21:55 +0530

    udc: dwc3: reset the event buffer pointer
    Reset the event buffer pointer to 0 in the enable() API. This resets the
    pointer in case of a disable() -> enable() cycle.
    
    Signed-off-by: Amneesh Singh <amneesh@ti.com>

--- drivers/usb/udc/udc_dwc3.c
+++ drivers/usb/udc/udc_dwc3.c
@@ -2035,6 +2035,8 @@ static int udc_dwc3_enable(const struct device *const dev)
 
 	LOG_INF("Enabling DWC3 driver");
 
+	priv->evt_next = 0;
+
 	ret = udc_dwc3_quirk_enable(dev);
 	if (ret != 0) {
 		return ret;
